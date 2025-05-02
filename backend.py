cab4d69fb9f54601f27f5e1ab3d66028312d16de
JaroslawHryszko <jarek.hryszko@gmail.com>
2025-05-02 07:58:02 +0200

    refactoring

--- backend.py
+++ backend.py
@@ -1,15 +1,40 @@
-from flask import Flask, request, render_template, redirect, url_for, Response
+from flask import Flask, request, render_template, redirect, url_for, Response, session, flash
 from uranus import Uranus
 from waitress import serve
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json, csv
+import uuid
+import pandas as pd
+import bcrypt
+from functools import wraps
 
 app = Flask(__name__)
+
+app.secret_key = 'SyagheKrRg3eygnoBfoEMybFzN90XJtG' 
+PASSWORD_HASH = bcrypt.hashpw(b"rowerjestbardzookej", bcrypt.gensalt())
+
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+global user_name
+user_name = "NULL"
+
+@app.context_processor
+def inject_globals():
+    return {'datetime': datetime, 'enumerate': enumerate, "getattr": getattr}
+
+def admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('admin_logged_in'):
+            return redirect(url_for('admin'))
+        return f(*args, **kwargs)
+    return decorated_function
+
 
 # Definicja modelu danych
 class PrioritizedRisks(db.Model):
@@ -31,105 +56,240 @@ class ClassicRisks(db.Model):
     impact = db.Column(db.Integer, nullable=False)
     probability = db.Column(db.Integer, nullable=False)
     risk_priority = db.Column(db.Integer, nullable=False)
-    
+   
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(36), unique=True, nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-def load_values_from_json(file_path):
-    with open(file_path, 'r') as file:
+
+class ClassicResults(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(36), db.ForeignKey('user.user_id'), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False)
+    end_time = db.Column(db.DateTime, nullable=False)
+    risk_id = db.Column(db.Integer, nullable=False)
+    risk_name = db.Column(db.Text, nullable=False)
+    impact = db.Column(db.Integer, nullable=False)
+    probability = db.Column(db.Integer, nullable=False)
+    priority = db.Column(db.Integer, nullable=False)
+
+
+class NoveltyResults(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(36), db.ForeignKey('user.user_id'), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False)
+    end_time = db.Column(db.DateTime, nullable=False)
+    risk_a_id = db.Column(db.Integer, nullable=False)
+    risk_a_description = db.Column(db.Text, nullable=False)
+    risk_b_id = db.Column(db.Integer, nullable=False)
+    risk_b_description = db.Column(db.Text, nullable=False)
+    chosen_risk = db.Column(db.String(1), nullable=False)
+    parameter = db.Column(db.String(255), nullable=False)
+
+
+def load_values_from_json(file_path, content):
+    with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
-    return data['values']
+        datatoreturn = data[content]
+    return datatoreturn
 
 file_path = 'config.json'
-values = load_values_from_json(file_path)
+values = load_values_from_json(file_path,'risks')
 
-global u, user_name, start_time
+global u, start_time
 u = Uranus(['impact', 'probability'], values)
 
-user_name = 'Anon'
 start_time = datetime.now()
 
-@app.route('/novelty_risk')
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    if request.method == 'POST':
+        password = request.form.get('password').encode('utf-8')
+        if bcrypt.checkpw(password, PASSWORD_HASH):
+            session['admin_logged_in'] = True
+            return redirect(url_for('admin_dashboard'))
+        else:
+            flash("Invalid password.", "danger")
+            return render_template('admin_login.html')
+    return render_template('admin_login.html')
+    
+
+@app.route('/admin/dashboard', methods=['GET', 'POST'])
+@admin_required
+def admin_dashboard():
+    data = []
+    table_name = None
+
+    if request.method == 'POST':
+        table_name = request.form.get('table')
+
+        if table_name == "ClassicResults":
+            data = ClassicResults.query.all()
+        elif table_name == "NoveltyResults":
+            data = NoveltyResults.query.all()
+
+    return render_template('admin_dashboard.html', table_name=table_name, data=data)
+
+@app.route('/admin/edit_config', methods=['GET', 'POST'])
+@admin_required
+def edit_config():
+    if not session.get('admin_logged_in'):
+        return redirect(url_for('admin'))
+
+    config_file = 'config.json'
+
+    if request.method == 'POST':
+        # Pobierz dane z formularza
+        new_values = request.form.get('config_data')
+        try:
+            # Walidacja poprawnosci JSON
+            parsed_json = json.loads(new_values)
+            # Zapisz nowy plik config.json
+            with open(config_file, 'w', encoding='utf-8') as f:
+                json.dump(parsed_json, f, indent=4)
+            flash('Config.json was saved successfully.', 'success')
+        except json.JSONDecodeError:
+            flash('JSON syntax error!', 'danger')
+        return redirect(url_for('edit_config'))
+
+    # Odczytaj aktualne dane z config.json
+    with open(config_file, 'r', encoding='utf-8') as f:
+        config_data = json.load(f)
+    # Konwertuj dane do ladnego formatu JSON
+    config_data_pretty = json.dumps(config_data, indent=4, ensure_ascii=False)
+    return render_template('edit_config.html', config_data=config_data_pretty)
+
+@app.route('/export/csv/<table_name>')
+@admin_required
+def export_csv(table_name):
+    timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+    if table_name == "ClassicResults":
+        data = ClassicResults.query.all()
+        columns = ["user_id", "start_time", "end_time", "risk_id", "risk_name", "impact", "probability", "priority"]
+    elif table_name == "NoveltyResults":
+        data = NoveltyResults.query.all()
+        columns = ["user_id", "start_time", "end_time", "risk_a_id", "risk_a_description", "risk_b_id", "risk_b_description", "chosen_risk", "parameter"]
+    else:
+        return "Invalid table name", 400
+
+    csv_data = pd.DataFrame([{col: getattr(row, col) for col in columns} for row in data]).to_csv(index=False)
+
+    response = Response(
+        csv_data,
+        mimetype="text/csv",
+        headers={"Content-disposition": f"attachment; filename={table_name}-{timestamp}.csv"}
+    )
+    return response
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+
+@app.route('/novelty_risk', methods=['GET', 'POST'])
 def novelty_risk():
-    global start_time
-    start_time = datetime.now()
+    global start_time, a, b, c
+    print('zaczynam')
+    if request.method == 'POST':
+        print('odebralem')
+        stop_time = datetime.now()
+        choice = int(request.form['choice'])
+        a = int(request.form['a'])
+        b = int(request.form['b'])
+        c = int(request.form['c'])
+        chosen = "A" if choice == 0 else "B"
+        result = NoveltyResults(
+            user_id=user_id,
+            start_time=start_time,
+            end_time=stop_time,
+            risk_a_id=a,
+            risk_a_description=u.e_names[a],
+            risk_b_id=b,
+            risk_b_description=u.e_names[b],
+            chosen_risk=chosen,
+            parameter=u.p_names[c]
+        )
+        db.session.add(result)
+        db.session.commit()
+        print(f"Saving to NoveltyResults: user_id={user_id}, risk_a_id={a}, risk_b_id={b}, chosen_risk={chosen}")
+        u.set_priority(choice)
+        start_time = stop_time
     a, b, c = u.next_to_process()
     if a is None or b is None or c is None:
-        # ponizszy licznik wskazuje priorytety, nie id ryzyk
-        counter = 1
-        stop_time = datetime.now()
-        for risk in u.prioritized_list():
-            new_record = PrioritizedRisks(starttime = start_time, stoptime=stop_time, username=user_name, risk_number=risk, risk_name=values[risk], risk_priority=counter)
-            db.session.add(new_record)
-            counter += 1
-        db.session.commit()
-        new_prioritized_list = u.prioritized_list()
         u.reset()
-        return render_template('all_done.html', prioritized_list=new_prioritized_list, values=values)
+        return render_template('all_done.html', values=values)
     return render_template('novelty_risk.html', a=a, b=b, c=c, p_names=u.p_names, e_names=u.e_names)
+
     
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    welcome_text = load_values_from_json(file_path,'welcome_text')
     if request.method == 'POST':
-        # Odbieranie danych z formularza
         retrieved_user_name = request.form['name']
-        if retrieved_user_name == 'zulugula':
-            return redirect(url_for('data_display'))
-        else:
-            global user_name
-            user_name = retrieved_user_name
-            return redirect(url_for('instruct'))  
-    return render_template('introduction.html')
 
-@app.route('/data_display')
-def data_display():
-    return render_template('data_display.html')
+        # Check if the user exists
+        user = User.query.filter_by(name=retrieved_user_name).first()
+        if not user:
+            # Create a new user with a UUID
+            user = User(user_id=str(uuid.uuid4()), name=retrieved_user_name)
+            db.session.add(user)
+            db.session.commit()
+
+        global user_name, user_id
+        user_name = user.name
+        user_id = user.user_id
+
+        return redirect(url_for('instruct'))
+    return render_template('introduction.html', welcome_text=welcome_text, user_name=user_name)
 
     
 @app.route('/instruct')
 def instruct():
-    return render_template('instruct.html')
+    instructions = load_values_from_json(file_path,'instructions')
+    return render_template('instruct.html', instructions=instructions)
 
-def myAss(essment):
-    return essment['assessment']
 
 @app.route('/classic_risk', methods=['GET', 'POST'])
 def classic_risk():
     global start_time
-    start_time = datetime.now()
+
+    if request.method == 'GET':
+        # Record the time when the page is displayed
+        start_time = datetime.now()
+
     if request.method == 'POST':
-        updated_values = []
-        # Adam liczy swoje ryzyka zaczynajac od zera
-        counter = 0
         stop_time = datetime.now()
+
         for risk in values:
-            assessed_probability = request.form.get(f'probability_{risk}', 0)
-            assessed_impact = request.form.get(f'impact_{risk}', 0)
-            assessed_probability_int = int(assessed_probability)
-            assessed_impact_int = int(assessed_impact)
-            calculated_assessment = assessed_probability_int * assessed_impact_int
-            updated_values.append({'risk': counter, 'assessment': calculated_assessment})
-            new_record = ClassicRisks(starttime = start_time, stoptime=stop_time, username=user_name, risk_number=counter, risk_name=risk, impact=assessed_impact_int, probability=assessed_probability_int, risk_priority = calculated_assessment)
-            db.session.add(new_record)
-            counter += 1
+            assessed_probability = int(request.form.get(f'probability_{risk}', 0))
+            assessed_impact = int(request.form.get(f'impact_{risk}', 0))
+            calculated_assessment = assessed_probability * assessed_impact
+
+            # Save the result to the database
+            result = ClassicResults(
+                user_id=user_id,
+                start_time=start_time,
+                end_time=stop_time,
+                risk_id=values.index(risk),
+                risk_name=risk,
+                impact=assessed_impact,
+                probability=assessed_probability,
+                priority=calculated_assessment
+            )
+            db.session.add(result)
+
         db.session.commit()
-        updated_values.sort(key=myAss, reverse=True)
-        new_prioritized_list = []
-        for updated_value in updated_values:
-            new_prioritized_list.append(updated_value['risk'])
-        global u
-        u.reset()
-        return render_template('all_done.html', prioritized_list=new_prioritized_list, values=values)
+
+        return render_template('all_done.html', risks=values)
+
     return render_template('classic_risk.html', risks=values)
 
 
 
-@app.route('/set_priority', methods=['POST'])
-def set_priority():
-    choice = request.form['choice']
-    if choice == '0':
-        u.set_priority(0)
-    elif choice == '1':
-        u.set_priority(1)
-    return redirect(url_for('novelty_risk'))
 
 @app.route('/reset', methods=['GET'])
 def reset():
@@ -137,37 +297,11 @@ def reset():
     u.reset()
     return redirect(url_for('index'))
 
-@app.route('/display_data')
-def display_data():
-    # Pobieranie wszystkich rekordów z bazy danych
-    items = PrioritizedRisks.query.all()
-    # Przekazywanie rekordów do szablonu HTML
-    return render_template('display_data.html', items=items)
-    
-@app.route('/display_classic_data')
-def display_classic_data():
-    # Pobieranie wszystkich rekordów z bazy danych
-    items = ClassicRisks.query.all()
-    # Przekazywanie rekordów do szablonu HTML
-    return render_template('display_classic_data.html', items=items)
-    
-@app.route('/export/csv')
-def export_csv():
-    # Pobranie danych
-    items = PrioritizedItem.query.all()
-    # Utworzenie pliku CSV
-    si = io.StringIO()
-    cw = csv.writer(si)
-    cw.writerow(['ID', 'Content', 'Start Time', 'End Time'])  # Nagłówki kolumn
-    for item in items:
-        cw.writerow([item.id, item.content, item.start_time, item.end_time])
 
-    output = make_response(si.getvalue())
-    output.headers["Content-Disposition"] = "attachment; filename=export.csv"
-    output.headers["Content-type"] = "text/csv"
-    return output
+
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()  # Tworzy tabelę w bazie danych przy pierwszym uruchomieniu
-        serve(app, host="0.0.0.0", port=80)
+    with app.app_context():       
+        db.create_all()  
+        #app.run(debug=True, host="0.0.0.0", port=80)
+        serve(app, host="127.0.0.1", port=80)
